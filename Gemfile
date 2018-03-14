3cb6d7cbe518cda9dc59c53514c4730d1537eee7
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2018-03-13 21:15:05 -0400

    chapter_8_basic_login

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "sqlite3"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
 gem "bcrypt", "~> 3.1.7"
+gem "jquery-rails"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "/help", to: "static_pages#help"
+  get "/signup",  to: "users#new"
+  resources :users
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
