c753c5773c4db9c17d5b00b668c6f8acb5259365
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2018-03-14 19:15:31 -0400

    Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,24 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+    log_in @user
+    params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+    redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render :new
+      render 'new'
     end
   end
 
   def destroy
     log_out
     redirect_to root_url
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
