5c50e3778dd86dcf51b9921aaa48f8d9b641d9f3
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2018-03-18 14:25:12 +0700

    advance login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save {self.email = email.downcase}
 
   validates :name, presence: true
@@ -14,5 +16,24 @@ class User < ApplicationRecord
 		BCrypt::Password.create(string, cost: cost)
 	end
 
-  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
 end
